dc0d5d1630dd94d30ca823d32fdacb858c98679b
UmitKok21 <umit.kok21@gmail.com>
2025-03-13 14:13:02 +0300

    Promise Example.

---
+++ js/Promise.js
@@ -0,0 +1,25 @@
+const res = (resource) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else if (request.readyState === 4) {
+          reject('Failed');
+        }
+      });
+  
+      request.open('GET', resource);
+      request.send();
+    });
+  };
+  
+  res('example/can.json')
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
